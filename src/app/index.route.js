80c1d87b22581ebdd2d4ec454c6647c4a011b939
mmszymczak <person.szymczak@gmail.com>
2016-04-12 14:46:08 +0200

    added simple form

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,25 +1,30 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
+    angular
     .module('project')
     .config(routeConfig);
 
-  function routeConfig($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      })
-      .when('/some', {
-        templateUrl: 'app/components/showResults/main.html',
-        controller: 'ResultsController',
-        controllerAs: 'result'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    function routeConfig($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'main'
+        })
+        .when('/category', {
+            templateUrl: 'app/components/showResults/main.html',
+            controller: 'ResultsController',
+            controllerAs: 'result'
+        })
+        .when('/login', {
+            templateUrl: 'app/components/loginWithoutFB/login.html',
+            controller: 'LoginController',
+            controllerAs: 'loginCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
   }
 
 })();
