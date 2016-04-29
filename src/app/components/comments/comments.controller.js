7968d3729be61ebf722e5e1679f6375b3beaed5a
mmszymczak <person.szymczak@gmail.com>
2016-04-29 13:04:08 +0200

    fixed comments block + validated form

--- src/app/components/comments/comments.controller.js
+++ src/app/components/comments/comments.controller.js
@@ -19,22 +19,31 @@
         elemComVm.commentRate = 3;
         elemComVm.sendComment = sendComment;
         elemComVm.commentsArray = [];
+        elemComVm.resetForm = resetForm;
+        elemComVm.submitted=true;
+        elemComVm.comment = {
+            text: '',
+            rate: 0,
+            surname: '',
+            name: ''
+        }
 
-        elemComVm.parcel = {
-            fullname: 'ziutek',
-            picture: 'https://scontent.xx.fbcdn.net/hprofile-xft1/v/t1.0-1/p50x50/12688296_1073512766013897_6249594735101307367_n.jpg?oh=c76c4b229088c7cbe83fa6076f2bee5f&oe=57B23B35',
-            date: makeDate(),
-            comment: 'testowyowy owy komentarzeee',
-            stars: 4
-        };
 
+        function resetForm() {
+            elemComVm.comment = {
+                text: '',
+                rate: 0,
+                surname: '',
+                name: ''
+            }
+        }
 
         function makeDate() {
             var d = new Date(),
                 year = d.getFullYear(),
-                month = d.getMonth(),
+                month = d.getMonth() + 1,
                 day = d.getDate(),
-                fullDate = day+'-'+month+1+'-'+year;
+                fullDate = year+'-'+month+'-'+day;
 
             return fullDate;
         }
@@ -42,8 +51,10 @@
         function sendComment(comment) {
             comment['picture'] = userService.user.picture;
             comment['date'] = makeDate();
+            elemComVm.submitted=false;
 
             firebaseService.addComment(elemComVm.actualPlace, comment);
+            resetForm();
         }
 
         function showComments() {
